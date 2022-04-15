5e24d18233e9823e43c0284aefa6e36e7903a1df
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-15 16:11:54 +0200

    html: class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,7 +53,7 @@ function Header() {
 {(isAuth) ? <div><nav>
 <ul>
    <li className=''><a href="http://localhost:3000/"className="img_header">{theUser.pseudo}</a>
-        <ul class="sous">
+        <ul className="sous">
             <li><Link to="Profil" className='a'>Profil</Link></li>
             <li><Link to="Parameters" className='a'>Paramétres</Link></li>
             <li><Link to='/'className='a' onClick={logOut}>Déconnexion</Link></li>
