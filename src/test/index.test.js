13cab258df7c88f852829fe51ef802c06a5f90a4
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-20 14:34:18 +0200

    test: display interface

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,7 +1,7 @@
 import React, {} from 'react';
 import Exercices from "../components/Exercices";
 import '@testing-library/jest-dom/extend-expect'
-import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ModulesProvider } from '../contexts/ModulesContext.jsx';
 import MyContextProvider from '../contexts/MyContext';
 import ReactRouter from 'react-router';
@@ -11,6 +11,11 @@ import { setupServer } from 'msw/node'
 const questionsResponse = rest.get("http://localhost/01-academie/src/server/ex_req_ajax.php", (req, res, ctx) => {
     return(res(ctx.json([{id:1, questions:"le HTML est un langage de: ", answer_one:"balisage", anszwer_two:"script", answer_three:"communication entre humain", good_answer:"balisage" }])))
 })
+
+const stubbedQuestions = [
+    { id: 1, questions: "le HTML est un langage de: " },
+    { id: 2, questions: "La balise <p> représente :" },
+  ];
 
 const handlers = [questionsResponse];
 
@@ -49,6 +54,7 @@ describe('Exercices', () => {
 
     test('should have 3 hearts', async () => {
         jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1' });
+        jest.spyOn(React, 'useState').mockReturnValue({ isLoadgin: false });
         render(
             <MyContextProvider>
             <ModulesProvider>
@@ -59,20 +65,4 @@ describe('Exercices', () => {
         const hearts = screen.getAllByTestId('heart-element')
         expect(hearts).toHaveLength(3);
     })
-/*
-    test('should display questions', async () => {
-        jest.spyOn(ReactRouter, 'useParams').mockReturnValue({ id: '1' });
-        jest.spyOn(React, 'useState').mockReturnValue({ heart: '3' });
-        render(
-            <MyContextProvider>
-            <ModulesProvider>
-                <Exercices />
-            </ModulesProvider>
-            </MyContextProvider>
-        )
-        const btn = screen.getAllByTestId('click-element')
-        const question = screen.findByText('balisage')
-        expect(question).toBeVisible();
-    })*/
-    
 })
