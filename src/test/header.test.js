88fd344eda9a84612f1765bd0fb08b10db4ef7dc
Guillaume David <guillaumedaavidpro@gmail.com>
2022-04-27 14:57:13 +0200

    test: should render without crashing

---
+++ src/test/header.test.js
@@ -0,0 +1,18 @@
+import React, {} from 'react';
+import Header from "../components/Header";
+import { render, screen } from '@testing-library/react';
+import MyContextProvider from '../contexts/MyContext';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter as Router } from "react-router-dom";
+
+describe('Header', () => {
+    test('Should render without crashing', async () => {
+        render(
+            <Router>
+            <MyContextProvider>
+                <Header />
+            </MyContextProvider>
+            </Router>
+        )
+    })
+})
